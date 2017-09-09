1b466c2e03abee4f0e4cd816b9763dae5bbdbcde
vitto <vittorio.camisa@gmail.com>
2017-09-09 15:46:07 +0200

    updated vimme.sh with vundle setup and lusty-eplorer package dependencies

--- vimme.sh
+++ vimme.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
+set -e
+
+# Setup vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# pre-requisite for lusty-explorer
+# debian
+apt-get install vim-gnome
 
 cp ./.vimrc ~/.vimrc
+
 vim -c PluginInstall
