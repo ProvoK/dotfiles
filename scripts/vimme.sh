f0b8000daaabfcd2fadc667102ca9929ff2bba9a
vitto <vittorio.camisa@gmail.com>
2017-11-18 16:45:31 +0100

    updated vimme with backup of old vimrc and sequentially executing PluginClean/Update/Install/quit

--- scripts/vimme.sh
+++ scripts/vimme.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 set -e
 
+# Backup vimrc
+cp ~/.vimrc ~/.vimrc_backup_`date +%F`
+
+# Copy new vimrc
 cp ./.vimrc ~/.vimrc
 
-vim -c PluginClean
-vim -c PluginUpdate
-vim -c PluginInstall
+# Vim update
+vim -c PluginClean -c PluginUpdate -c PluginInstall -c qa
