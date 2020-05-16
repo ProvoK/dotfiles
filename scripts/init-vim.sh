7e4a4404052822d600504c9ad9e4aa38b1fe110c
Vittorio Camisa <vittorio.camisa@sysdig.com>
2020-05-16 11:36:24 +0200

    change: deleted deprecated scripts

--- scripts/init-vim.sh
+++
@@ -1,9 +0,0 @@
-# Setup vundle
-rm -rf ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# pre-requisite for lusty-explorer
-# debian
-sudo apt-get install vim-gnome
-
-
